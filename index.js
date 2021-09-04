05978b0c422eb763ffd202af0121a539e9fe7961
Arshdeep <arshsingh3223@gmail.com>
2021-09-04 12:55:15 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+function superbowlWin(array) {
+ const footBall = array.find (game => game.result === "W")
+//    if (footBall){
+//     return footBall.year
+//    } else {
+//        return footBall
+//    }
 
+ return footBall ?  footBall.year : footBall
+
+}
